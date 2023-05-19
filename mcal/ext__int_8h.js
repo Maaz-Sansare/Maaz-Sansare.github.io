2a68ed345a95c7d814f5d65f4c5055973131348a
Maaz Sansare <maaz.sansare@phi-robotics.com>
2023-05-19 18:53:47 +0530

    minor mainpage change in mcal

--- mcal/ext__int_8h.js
+++ mcal/ext__int_8h.js
@@ -1,16 +1,16 @@
 var ext__int_8h =
 [
-    [ "pcf_ExtInt_getTotalIds", "group__pcf__mcal__peripherals__ext__int.html#gabfea13ba45a51f1145d477cde6b3cd8d", null ],
-    [ "pcf_ExtInt_getAvailableIds", "group__pcf__mcal__peripherals__ext__int.html#ga981c9e83dfbbdfa9ed29f4e9ba63061b", null ],
-    [ "pcf_ExtInt_new", "group__pcf__mcal__peripherals__ext__int.html#ga627e52529f6e4a12502c5406cb4931ab", null ],
+    [ "pcf_ExtInt_getTotalIds", "group__pcf__mcal__peripherals__ext__int.html#gaa837bb98f2d61f412f08db30bdb01382", null ],
+    [ "pcf_ExtInt_getAvailableIds", "group__pcf__mcal__peripherals__ext__int.html#ga97e9d1ec79c11307a05c0a0593e61f71", null ],
+    [ "pcf_ExtInt_new", "group__pcf__mcal__peripherals__ext__int.html#gac08dac0af00c92b54948a03e455d9e9c", null ],
     [ "pcf_ExtInt_delete", "group__pcf__mcal__peripherals__ext__int.html#gaaa8bd163e5ce968921c82c6b637fe824", null ],
     [ "pcf_ExtInt_enable", "group__pcf__mcal__peripherals__ext__int.html#gad20d316f21337b22c4a30104af4676f1", null ],
     [ "pcf_ExtInt_disable", "group__pcf__mcal__peripherals__ext__int.html#gaa9f83d8a1cd0e8564509f5215116df93", null ],
     [ "pcf_ExtInt_attachCallback", "group__pcf__mcal__peripherals__ext__int.html#ga169986e0e9938101504201af96316192", null ],
     [ "pcf_ExtInt_detachCallback", "group__pcf__mcal__peripherals__ext__int.html#ga4658f139322a854eb58380506562099e", null ],
     [ "pcf_ExtInt_getPortPin", "group__pcf__mcal__peripherals__ext__int.html#gaf5981adece699118a9372bf1c88b3b43", null ],
-    [ "pcf_ExtInt_getTrigger", "group__pcf__mcal__peripherals__ext__int.html#ga410d47c3428d58f290e56b3b176cbea7", null ],
-    [ "pcf_ExtInt_setPriority", "group__pcf__mcal__peripherals__ext__int.html#ga9c3cdc245bc5dde232b62ef7610ab9ec", null ],
-    [ "pcf_ExtInt_getPriority", "group__pcf__mcal__peripherals__ext__int.html#ga7fb10a4ce14c74605f160f1c31c3ab6c", null ],
-    [ "pcf_ExtInt_customCommand", "group__pcf__mcal__peripherals__ext__int.html#gaab5bc229db5f8698acbf45b170446342", null ]
+    [ "pcf_ExtInt_getTrigger", "group__pcf__mcal__peripherals__ext__int.html#ga72426b937bcde527aab0b582a1c320c9", null ],
+    [ "pcf_ExtInt_setPriority", "group__pcf__mcal__peripherals__ext__int.html#ga60b58701b4c62c38cb4a4fdf73e28273", null ],
+    [ "pcf_ExtInt_getPriority", "group__pcf__mcal__peripherals__ext__int.html#ga64c0f7467b38021f9ff321527c2d0e79", null ],
+    [ "pcf_ExtInt_customCommand", "group__pcf__mcal__peripherals__ext__int.html#ga2ff6200bed59b8d8f83957d8164ff409", null ]
 ];
